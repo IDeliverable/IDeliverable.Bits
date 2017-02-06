fd9a163ba0c318dc91874e974291a9312b92e75e
Daniel Stolt <daniel@ideliverable.com>
2017-02-06 23:04:22 +0100

    Added snippet field type alternates.

--- Layouts/Shapes/ElementShapeAlternates.cs
+++ Layouts/Shapes/ElementShapeAlternates.cs
@@ -1,8 +1,10 @@
 ﻿using Orchard.DisplayManagement.Descriptors;
 using Orchard.Environment.Extensions;
 using Orchard.Layouts.Framework.Elements;
+using System;
 
-namespace IDeliverable.Bits.Navigation.Shapes {
+namespace IDeliverable.Bits.Navigation.Shapes
+{
     [OrchardFeature("IDeliverable.Bits.Layouts")]
     public class ElementShapeAlternates : IShapeTableProvider
     {
@@ -13,10 +15,16 @@ namespace IDeliverable.Bits.Navigation.Shapes {
                 var element = (Element)context.Shape.Element;
                 var typeName = element.GetType().Name;
 
-                if (element.Container != null) {
+                if (element.Container != null)
+                {
                     var containerTypeName = element.Container.GetType().Name;
                     context.ShapeMetadata.Alternates.Add($"Elements_{typeName}__Parent__{containerTypeName}");
                 }
+            });
+
+            builder.Describe("Elements_Snippet_Field_Design").OnDisplaying(context =>
+            {
+                context.ShapeMetadata.Alternates.Add(String.Format("Elements_Snippet_Field_Design__{0}", context.Shape.Type));
             });
         }
     }
