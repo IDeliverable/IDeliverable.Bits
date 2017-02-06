cfcc5a63e353e2f1e36b16e08f479894f0e50c8a
Daniel Stolt <daniel@ideliverable.com>
2017-02-06 23:26:02 +0100

    Added PartialAliasRoute and associated helper.

---
+++ Routing/PartialAliasHelper.cs
@@ -0,0 +1,50 @@
+﻿using Orchard.Alias.Implementation.Holder;
+using Orchard.Alias.Implementation.Map;
+using System;
+using System.Linq;
+
+namespace IDeliverable.Bits.Routing
+{
+	public static class PartialAliasHelper
+	{
+		public static AliasInfo GetAlias(string requestPath, IAliasHolder aliasHolder)
+		{
+			var alias = GetClosestParentAlias(requestPath, aliasHolder.GetMap("Contents"), true);
+			if (alias != null)
+				return alias;
+
+			foreach (var map in aliasHolder.GetMaps())
+			{
+				alias = GetClosestParentAlias(requestPath, map, true);
+				if (alias != null)
+					return alias;
+			}
+
+			return null;
+		}
+
+		public static AliasInfo GetClosestParentAlias(string virtualPath, AliasMap aliasMap, bool considerCurrentPath)
+		{
+			var aliases = aliasMap.GetAliases();
+			var path = considerCurrentPath ? virtualPath : RemoveOneSubdirectoryFromPath(virtualPath);
+			AliasInfo aliasInfo = null;
+
+			while (path.Length > 0)
+			{
+				aliasInfo = aliases.Where(x => x.Path == path).SingleOrDefault();
+
+				if (aliasInfo != null)
+					break;
+
+				path = RemoveOneSubdirectoryFromPath(path);
+			}
+
+			return aliasInfo;
+		}
+
+		private static string RemoveOneSubdirectoryFromPath(string path)
+		{
+			return String.Join("/", path.Split('/').Reverse().Skip(1).Reverse());
+		}
+	}
+}
